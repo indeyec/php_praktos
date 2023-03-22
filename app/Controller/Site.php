4519035376fd7ac86279606a23d508ec8b85b891
Vladislav Shilov <vlad.shilov03@mail.ru>
2023-03-22 22:52:03 +0700

    sdfdfgd

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -9,16 +9,16 @@ use Src\Request;
 class Site
 {
     public function index(Request $request): string
-{
-   $posts = Post::where('id', $request->id)->get();
-   return (new View())->render('site.post', ['posts' => $posts]);
-}
+    {
+       $posts = Post::where('id', $request->id)->get();
+       return (new View())->render('site.post', ['posts' => $posts]);
+    }
+    
 
    public function hello(): string
    {
        return new View('site.hello', ['message' => 'hello working']);
    }
-   
-  
-
 }
+
+
