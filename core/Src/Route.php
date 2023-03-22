91470e6b9131ea302c962679c93402f66b3fd02f
Vladislav Shilov <vlad.shilov03@mail.ru>
2023-03-22 23:13:02 +0700

    аутентификация

--- core/Src/Route.php
+++ core/Src/Route.php
@@ -20,6 +20,21 @@ class Route
            self::$routes[$route] = $action;
        }
    }
+   public function redirect(string $url): void
+{
+   header('Location: ' . $this->getUrl($url));
+}
+
+public function getUrl(string $url): string
+{
+   return self::$prefix . $url;
+}
+
+public function __construct(string $prefix = '')
+{
+   self::setPrefix($prefix);
+}
+
 
    public function start(): void
    {
